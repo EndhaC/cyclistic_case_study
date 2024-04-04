5708441b64c496a34fb3ae349d3c65113fdae006
Endha <121072673+EndhaC@users.noreply.github.com>
2024-04-04 10:02:54 +0700

    Update code_collections.sql

--- code_collections.sql
+++ code_collections.sql
@@ -15,3 +15,20 @@ WHERE ride_id IS NOT NULL AND
       end_lat IS NOT NULL AND
       end_lng IS NOT NULL AND
       member_casual IS NOT NULL
+
+ /* Removing Whitespaces in the dataset */     
+
+UPDATE `analyze-data-12345.cyclistic_datasets.cyclistic_2023_CleanedDataset` 
+SET ride_id  = TRIM(ride_id). 
+    rideable_type = TRIM(rideable_type), 
+    started_at = TRIM(started_at), 
+    ended_at = TRIM(ended_at), 
+    start_station_name = TRIM(start_station_name), 
+    start_station_id = TRIM(start_station_id), 
+    end_station_name = TRIM(end_station_name), 
+    end_station_id = TRIM(end_station_id), 
+    start_lat = TRIM(start_lat), 
+    start_lng = TRIM(start_lng), 
+    end_lat = TRIM(end_lat), 
+    end_lng = TRIM(end_lng), 
+    member_casual = TRIM(member_casual)
